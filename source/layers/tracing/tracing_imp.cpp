d4cf1071a701e6007acda35b4b60d59af79f3792
davidoli <david.olien@intel.com>
2020-11-16 18:46:32 -0800

    Update formatting.
    Signed-off-by: davidoli <david.olien@intel.com>

--- source/layers/tracing/tracing_imp.cpp
+++ source/layers/tracing/tracing_imp.cpp
@@ -16,125 +16,125 @@ struct APITracerContextImp *pGlobalAPITracerContextImp =
     &globalAPITracerContextImp;
 
 APITracer *APITracer::create() {
-  APITracerImp *tracer = new APITracerImp;
-  tracer->tracingState = disabledState;
-  tracer->tracerFunctions = {};
-  UNRECOVERABLE_IF(tracer == nullptr);
-  return tracer;
+    APITracerImp *tracer = new APITracerImp;
+    tracer->tracingState = disabledState;
+    tracer->tracerFunctions = {};
+    UNRECOVERABLE_IF(tracer == nullptr);
+    return tracer;
 }
 
 ze_result_t createAPITracer(const zel_tracer_desc_t *desc,
                             zel_tracer_handle_t *phTracer) {
 
-  if (!pGlobalAPITracerContextImp->isTracingEnabled()) {
-    return ZE_RESULT_ERROR_UNINITIALIZED;
-  }
+    if (!pGlobalAPITracerContextImp->isTracingEnabled()) {
+        return ZE_RESULT_ERROR_UNINITIALIZED;
+    }
 
-  APITracerImp *tracer = static_cast<APITracerImp *>(APITracer::create());
+    APITracerImp *tracer = static_cast<APITracerImp *>(APITracer::create());
 
-  tracer->tracerFunctions.pUserData = desc->pUserData;
+    tracer->tracerFunctions.pUserData = desc->pUserData;
 
-  *phTracer = tracer->toHandle();
-  return ZE_RESULT_SUCCESS;
+    *phTracer = tracer->toHandle();
+    return ZE_RESULT_SUCCESS;
 }
 
 // This destructor will be called only during at-exit processing,
 // Hence, this function is executing in a single threaded environment,
 // and requires no mutex.
 APITracerContextImp::~APITracerContextImp() {
-  std::list<ThreadPrivateTracerData *>::iterator itr =
-      threadTracerDataList.begin();
-  while (itr != threadTracerDataList.end()) {
-    (*itr)->clearThreadTracerDataOnList();
-    itr = threadTracerDataList.erase(itr);
-  }
+    std::list<ThreadPrivateTracerData *>::iterator itr =
+        threadTracerDataList.begin();
+    while (itr != threadTracerDataList.end()) {
+        (*itr)->clearThreadTracerDataOnList();
+        itr = threadTracerDataList.erase(itr);
+    }
 }
 
 ze_result_t APITracerImp::destroyTracer(zel_tracer_handle_t phTracer) {
 
-  APITracerImp *tracer = static_cast<APITracerImp *>(phTracer);
+    APITracerImp *tracer = static_cast<APITracerImp *>(phTracer);
 
-  ze_result_t result =
-      pGlobalAPITracerContextImp->finalizeDisableImpTracingWait(tracer);
-  if (result == ZE_RESULT_SUCCESS) {
-    delete tracing_layer::APITracer::fromHandle(phTracer);
-  }
-  return result;
+    ze_result_t result =
+        pGlobalAPITracerContextImp->finalizeDisableImpTracingWait(tracer);
+    if (result == ZE_RESULT_SUCCESS) {
+        delete tracing_layer::APITracer::fromHandle(phTracer);
+    }
+    return result;
 }
 
 ze_result_t APITracerImp::setPrologues(zel_core_callbacks_t *pCoreCbs) {
 
-  if (this->tracingState != disabledState) {
-    return ZE_RESULT_ERROR_INVALID_ARGUMENT;
-  }
+    if (this->tracingState != disabledState) {
+        return ZE_RESULT_ERROR_INVALID_ARGUMENT;
+    }
 
-  this->tracerFunctions.corePrologues = *pCoreCbs;
+    this->tracerFunctions.corePrologues = *pCoreCbs;
 
-  return ZE_RESULT_SUCCESS;
+    return ZE_RESULT_SUCCESS;
 }
 
 ze_result_t APITracerImp::setEpilogues(zel_core_callbacks_t *pCoreCbs) {
 
-  if (this->tracingState != disabledState) {
-    return ZE_RESULT_ERROR_INVALID_ARGUMENT;
-  }
+    if (this->tracingState != disabledState) {
+        return ZE_RESULT_ERROR_INVALID_ARGUMENT;
+    }
 
-  this->tracerFunctions.coreEpilogues = *pCoreCbs;
+    this->tracerFunctions.coreEpilogues = *pCoreCbs;
 
-  return ZE_RESULT_SUCCESS;
+    return ZE_RESULT_SUCCESS;
 }
 
 ze_result_t APITracerImp::enableTracer(ze_bool_t enable) {
-  return pGlobalAPITracerContextImp->enableTracingImp(this, enable);
+    return pGlobalAPITracerContextImp->enableTracingImp(this, enable);
 }
 
 void APITracerContextImp::addThreadTracerDataToList(
     ThreadPrivateTracerData *threadDataP) {
-  std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
-  threadTracerDataList.push_back(threadDataP);
+    std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
+    threadTracerDataList.push_back(threadDataP);
 }
 
 void APITracerContextImp::removeThreadTracerDataFromList(
     ThreadPrivateTracerData *threadDataP) {
-  std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
-  if (threadTracerDataList.empty())
-    return;
-  threadTracerDataList.remove(threadDataP);
+    std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
+    if (threadTracerDataList.empty())
+        return;
+    threadTracerDataList.remove(threadDataP);
 }
 
 thread_local ThreadPrivateTracerData myThreadPrivateTracerData;
 
 ThreadPrivateTracerData::ThreadPrivateTracerData() {
-  isInitialized = false;
-  onList = false;
-  tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
+    isInitialized = false;
+    onList = false;
+    tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
 }
 
 ThreadPrivateTracerData::~ThreadPrivateTracerData() {
-  if (onList) {
-    globalAPITracerContextImp.removeThreadTracerDataFromList(this);
-    onList = false;
-  }
-  tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
+    if (onList) {
+        globalAPITracerContextImp.removeThreadTracerDataFromList(this);
+        onList = false;
+    }
+    tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
 }
 
 void ThreadPrivateTracerData::removeThreadTracerDataFromList(void) {
-  if (onList) {
-    globalAPITracerContextImp.removeThreadTracerDataFromList(this);
-    onList = false;
-  }
-  tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
+    if (onList) {
+        globalAPITracerContextImp.removeThreadTracerDataFromList(this);
+        onList = false;
+    }
+    tracerArrayPointer.store(nullptr, std::memory_order_relaxed);
 }
 
 bool ThreadPrivateTracerData::testAndSetThreadTracerDataInitializedAndOnList(
     void) {
-  if (!isInitialized) {
-    isInitialized = true;
-    onList = true;
-    globalAPITracerContextImp.addThreadTracerDataToList(
-        &myThreadPrivateTracerData);
-  }
-  return onList;
+    if (!isInitialized) {
+        isInitialized = true;
+        onList = true;
+        globalAPITracerContextImp.addThreadTracerDataToList(
+            &myThreadPrivateTracerData);
+    }
+    return onList;
 }
 
 // bool APITracerContextImp::isTracingEnabled() { return
@@ -149,15 +149,15 @@ bool APITracerContextImp::isTracingEnabled() { return true; }
 //
 ze_bool_t
 APITracerContextImp::testForTracerArrayReferences(tracer_array_t *tracerArray) {
-  std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
-  std::list<ThreadPrivateTracerData *>::iterator itr;
-  for (itr = threadTracerDataList.begin(); itr != threadTracerDataList.end();
-       itr++) {
-    if ((*itr)->tracerArrayPointer.load(std::memory_order_relaxed) ==
-        tracerArray)
-      return 1;
-  }
-  return 0;
+    std::lock_guard<std::mutex> lock(threadTracerDataListMutex);
+    std::list<ThreadPrivateTracerData *>::iterator itr;
+    for (itr = threadTracerDataList.begin(); itr != threadTracerDataList.end();
+         itr++) {
+        if ((*itr)->tracerArrayPointer.load(std::memory_order_relaxed) ==
+            tracerArray)
+            return 1;
+    }
+    return 0;
 }
 
 //
@@ -168,101 +168,101 @@ APITracerContextImp::testForTracerArrayReferences(tracer_array_t *tracerArray) {
 // Return the number of entries on the retiring tracer array list.
 //
 size_t APITracerContextImp::testAndFreeRetiredTracers() {
-  std::list<tracer_array_t *>::iterator itr =
-      this->retiringTracerArrayList.begin();
-  while (itr != this->retiringTracerArrayList.end()) {
-    tracer_array_t *retiringTracerArray = *itr;
-    itr++;
-    if (testForTracerArrayReferences(retiringTracerArray))
-      continue;
-    this->retiringTracerArrayList.remove(retiringTracerArray);
-    delete[] retiringTracerArray->tracerArrayEntries;
-    delete retiringTracerArray;
-  }
-  return this->retiringTracerArrayList.size();
+    std::list<tracer_array_t *>::iterator itr =
+        this->retiringTracerArrayList.begin();
+    while (itr != this->retiringTracerArrayList.end()) {
+        tracer_array_t *retiringTracerArray = *itr;
+        itr++;
+        if (testForTracerArrayReferences(retiringTracerArray))
+            continue;
+        this->retiringTracerArrayList.remove(retiringTracerArray);
+        delete[] retiringTracerArray->tracerArrayEntries;
+        delete retiringTracerArray;
+    }
+    return this->retiringTracerArrayList.size();
 }
 
 int APITracerContextImp::updateTracerArrays() {
-  tracer_array_t *newTracerArray;
-  size_t newTracerArrayCount = this->enabledTracerImpList.size();
+    tracer_array_t *newTracerArray;
+    size_t newTracerArrayCount = this->enabledTracerImpList.size();
 
-  if (newTracerArrayCount != 0) {
+    if (newTracerArrayCount != 0) {
 
-    newTracerArray = new tracer_array_t;
+        newTracerArray = new tracer_array_t;
 
-    newTracerArray->tracerArrayCount = newTracerArrayCount;
-    newTracerArray->tracerArrayEntries =
-        new tracer_array_entry_t[newTracerArrayCount];
-    //
-    // iterate over the list of enabled tracers, copying their entries into the
-    // new tracer array
-    //
-    size_t i = 0;
-    std::list<struct APITracerImp *>::iterator itr;
-    for (itr = enabledTracerImpList.begin(); itr != enabledTracerImpList.end();
-         itr++) {
-      newTracerArray->tracerArrayEntries[i] = (*itr)->tracerFunctions;
-      i++;
+        newTracerArray->tracerArrayCount = newTracerArrayCount;
+        newTracerArray->tracerArrayEntries =
+            new tracer_array_entry_t[newTracerArrayCount];
+        //
+        // iterate over the list of enabled tracers, copying their entries into the
+        // new tracer array
+        //
+        size_t i = 0;
+        std::list<struct APITracerImp *>::iterator itr;
+        for (itr = enabledTracerImpList.begin(); itr != enabledTracerImpList.end();
+             itr++) {
+            newTracerArray->tracerArrayEntries[i] = (*itr)->tracerFunctions;
+            i++;
+        }
+
+    } else {
+        newTracerArray = &emptyTracerArray;
     }
+    //
+    // active_tracer_array.load can use memory_order_relaxed here because
+    // there is logically no transfer of other memory context between
+    // threads in this case.
+    //
+    tracer_array_t *active_tracer_array_shadow =
+        activeTracerArray.load(std::memory_order_relaxed);
+    if (active_tracer_array_shadow != &emptyTracerArray) {
+        retiringTracerArrayList.push_back(active_tracer_array_shadow);
+    }
+    //
+    // This active_tracer_array.store must use memory_order_release.
+    // This store DOES signal a logical transfer of tracer state information
+    // from this thread to the tracing threads.
+    //
+    activeTracerArray.store(newTracerArray, std::memory_order_release);
+    testAndFreeRetiredTracers();
 
-  } else {
-    newTracerArray = &emptyTracerArray;
-  }
-  //
-  // active_tracer_array.load can use memory_order_relaxed here because
-  // there is logically no transfer of other memory context between
-  // threads in this case.
-  //
-  tracer_array_t *active_tracer_array_shadow =
-      activeTracerArray.load(std::memory_order_relaxed);
-  if (active_tracer_array_shadow != &emptyTracerArray) {
-    retiringTracerArrayList.push_back(active_tracer_array_shadow);
-  }
-  //
-  // This active_tracer_array.store must use memory_order_release.
-  // This store DOES signal a logical transfer of tracer state information
-  // from this thread to the tracing threads.
-  //
-  activeTracerArray.store(newTracerArray, std::memory_order_release);
-  testAndFreeRetiredTracers();
-
-  return 0;
+    return 0;
 }
 
 ze_result_t
 APITracerContextImp::enableTracingImp(struct APITracerImp *tracerImp,
                                       ze_bool_t enable) {
-  std::lock_guard<std::mutex> lock(traceTableMutex);
-  ze_result_t result;
-  switch (tracerImp->tracingState) {
-  case disabledState:
-    if (enable) {
-      enabledTracerImpList.push_back(tracerImp);
-      tracerImp->tracingState = enabledState;
-      updateTracerArrays();
+    std::lock_guard<std::mutex> lock(traceTableMutex);
+    ze_result_t result;
+    switch (tracerImp->tracingState) {
+    case disabledState:
+        if (enable) {
+            enabledTracerImpList.push_back(tracerImp);
+            tracerImp->tracingState = enabledState;
+            updateTracerArrays();
+        }
+        result = ZE_RESULT_SUCCESS;
+        break;
+
+    case enabledState:
+        if (!enable) {
+            enabledTracerImpList.remove(tracerImp);
+            tracerImp->tracingState = disabledWaitingState;
+            updateTracerArrays();
+        }
+        result = ZE_RESULT_SUCCESS;
+        break;
+
+    case disabledWaitingState:
+        result = ZE_RESULT_ERROR_UNINITIALIZED;
+        break;
+
+    default:
+        result = ZE_RESULT_ERROR_UNINITIALIZED;
+        UNRECOVERABLE_IF(true);
+        break;
     }
-    result = ZE_RESULT_SUCCESS;
-    break;
-
-  case enabledState:
-    if (!enable) {
-      enabledTracerImpList.remove(tracerImp);
-      tracerImp->tracingState = disabledWaitingState;
-      updateTracerArrays();
-    }
-    result = ZE_RESULT_SUCCESS;
-    break;
-
-  case disabledWaitingState:
-    result = ZE_RESULT_ERROR_UNINITIALIZED;
-    break;
-
-  default:
-    result = ZE_RESULT_ERROR_UNINITIALIZED;
-    UNRECOVERABLE_IF(true);
-    break;
-  }
-  return result;
+    return result;
 }
 
 // This is called by the destroy tracer method.
@@ -281,58 +281,58 @@ APITracerContextImp::enableTracingImp(struct APITracerImp *tracerImp,
 //
 ze_result_t APITracerContextImp::finalizeDisableImpTracingWait(
     struct APITracerImp *tracerImp) {
-  std::lock_guard<std::mutex> lock(traceTableMutex);
-  ze_result_t result;
-  switch (tracerImp->tracingState) {
-  case disabledState:
-    result = ZE_RESULT_SUCCESS;
-    break;
+    std::lock_guard<std::mutex> lock(traceTableMutex);
+    ze_result_t result;
+    switch (tracerImp->tracingState) {
+    case disabledState:
+        result = ZE_RESULT_SUCCESS;
+        break;
 
-  case enabledState:
-    result = ZE_RESULT_ERROR_UNINITIALIZED;
-    break;
+    case enabledState:
+        result = ZE_RESULT_ERROR_UNINITIALIZED;
+        break;
 
-  case disabledWaitingState:
-    while (testAndFreeRetiredTracers() != 0) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    case disabledWaitingState:
+        while (testAndFreeRetiredTracers() != 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+        tracerImp->tracingState = disabledState;
+        result = ZE_RESULT_SUCCESS;
+        break;
+
+    default:
+        result = ZE_RESULT_ERROR_UNINITIALIZED;
+        UNRECOVERABLE_IF(true);
+        break;
     }
-    tracerImp->tracingState = disabledState;
-    result = ZE_RESULT_SUCCESS;
-    break;
 
-  default:
-    result = ZE_RESULT_ERROR_UNINITIALIZED;
-    UNRECOVERABLE_IF(true);
-    break;
-  }
-
-  return result;
+    return result;
 }
 
 void *APITracerContextImp::getActiveTracersList() {
-  tracer_array_t *stableTracerArray = nullptr;
+    tracer_array_t *stableTracerArray = nullptr;
 
-  if (!myThreadPrivateTracerData
-           .testAndSetThreadTracerDataInitializedAndOnList()) {
-    return nullptr;
-  }
+    if (!myThreadPrivateTracerData
+             .testAndSetThreadTracerDataInitializedAndOnList()) {
+        return nullptr;
+    }
 
-  do {
-    stableTracerArray = pGlobalAPITracerContextImp->activeTracerArray.load(
-        std::memory_order_acquire);
-    myThreadPrivateTracerData.tracerArrayPointer.store(
-        stableTracerArray, std::memory_order_relaxed);
-  } while (stableTracerArray !=
-           pGlobalAPITracerContextImp->activeTracerArray.load(
-               std::memory_order_relaxed));
-  return (void *)stableTracerArray;
+    do {
+        stableTracerArray = pGlobalAPITracerContextImp->activeTracerArray.load(
+            std::memory_order_acquire);
+        myThreadPrivateTracerData.tracerArrayPointer.store(
+            stableTracerArray, std::memory_order_relaxed);
+    } while (stableTracerArray !=
+             pGlobalAPITracerContextImp->activeTracerArray.load(
+                 std::memory_order_relaxed));
+    return (void *)stableTracerArray;
 }
 
 void APITracerContextImp::releaseActivetracersList() {
-  if (myThreadPrivateTracerData
-          .testAndSetThreadTracerDataInitializedAndOnList())
-    myThreadPrivateTracerData.tracerArrayPointer.store(
-        nullptr, std::memory_order_relaxed);
+    if (myThreadPrivateTracerData
+            .testAndSetThreadTracerDataInitializedAndOnList())
+        myThreadPrivateTracerData.tracerArrayPointer.store(
+            nullptr, std::memory_order_relaxed);
 }
 
 } // namespace tracing_layer
