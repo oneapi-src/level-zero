ab4fcf54906f2c2bf760e9f1b6559561a5f7f1da
Neil R. Spruit <neil.r.spruit@intel.com>
2025-02-19 10:32:36 -0800

    Force dynamic init of L0 loader
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- source/lib/ze_lib.cpp
+++ source/lib/ze_lib.cpp
@@ -66,6 +66,38 @@ namespace ze_lib
             debug_trace_message(message, to_string(result));
             return result;
         }
+
+        // Init Dynamic Loader's Lib Context:
+        auto initDriversLoader = reinterpret_cast<ze_pfnInitDrivers_t>(
+            GET_FUNCTION_PTR(loader, "zeInitDrivers") );
+        auto initLoader = reinterpret_cast<ze_pfnInit_t>(
+            GET_FUNCTION_PTR(loader, "zeInit") );
+        if (initDriversLoader == nullptr && initLoader == nullptr) {
+            std::string message = "ze_lib Context Init() zeInitDrivers and zeInit missing, returning ";
+            debug_trace_message(message, to_string(ZE_RESULT_ERROR_UNINITIALIZED));
+            return ZE_RESULT_ERROR_UNINITIALIZED;
+        }
+        if (initDriversLoader != nullptr) {
+            uint32_t pInitDriversCount = 0;
+            result = initDriversLoader(&pInitDriversCount, nullptr, desc);
+        } else {
+            ze_init_flags_t init_flags = flags;
+            if (desc) {
+                if(desc->flags & ZE_INIT_DRIVER_TYPE_FLAG_GPU) {
+                    init_flags = ZE_INIT_FLAG_GPU_ONLY;
+                } else if(desc->flags & ZE_INIT_DRIVER_TYPE_FLAG_NPU) {
+                    init_flags = ZE_INIT_FLAG_VPU_ONLY;
+                } else {
+                    init_flags = 0;
+                }
+            }
+            result = initLoader(init_flags);
+        }
+        if (result != ZE_RESULT_SUCCESS) {
+            std::string message = "ze_lib Context Init() zeInitDrivers or zeInit failed with ";
+            debug_trace_message(message, to_string(result));
+            return result;
+        }
         typedef HMODULE (ZE_APICALL *getTracing_t)();
         auto getTracing = reinterpret_cast<getTracing_t>(
             GET_FUNCTION_PTR(loader, "zeLoaderGetTracingHandle") );
