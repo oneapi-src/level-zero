59b4c14ee190993362f2ad0b18ba841a0bceeb45
Neil R. Spruit <neil.r.spruit@intel.com>
2025-03-03 12:50:19 -0800

    Updated installer to only install SDK and added path gen script
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

---
+++ scripts/level_zero_sdk_setup.ps1
@@ -0,0 +1,24 @@
+"""
+ Copyright (C) 2025 Intel Corporation
+
+ SPDX-License-Identifier: MIT
+
+"""
+
+param (
+    [string]$sdkVersion,
+    [string]$installPath
+)
+
+# Define the paths
+$sdkPath = Join-Path -Path $installPath -ChildPath "LevelZeroSDK\$sdkVersion\"
+
+# Update or set the LEVEL_ZERO_SDK_V<major_version>_PATH environment variable
+$majorVersion = $sdkVersion.Split('.')[0]
+$envVarName = "LEVEL_ZERO_SDK_V${majorVersion}_PATH"
+$currentEnvPath = [System.Environment]::GetEnvironmentVariable($envVarName, [System.EnvironmentVariableTarget]::Machine)
+if ($null -eq $currentEnvPath -or $currentEnvPath -notcontains $sdkPath) {
+    [System.Environment]::SetEnvironmentVariable($envVarName, $sdkPath, [System.EnvironmentVariableTarget]::Machine)
+}
+
+Write-Output "Level Zero SDK Path Setup completed successfully."
