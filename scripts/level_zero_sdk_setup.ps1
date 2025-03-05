f39de16afbca789958c25b060babd6d894b2d293
Neil R. Spruit <neil.r.spruit@intel.com>
2025-03-05 15:24:41 -0800

    Fix Env setup, Icons, and side by side installation
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- scripts/level_zero_sdk_setup.ps1
+++ scripts/level_zero_sdk_setup.ps1
@@ -1,8 +1,6 @@
 """
  Copyright (C) 2025 Intel Corporation
-
  SPDX-License-Identifier: MIT
-
 """
 
 param (
@@ -15,7 +13,7 @@ $sdkPath = Join-Path -Path $installPath -ChildPath "LevelZeroSDK\$sdkVersion\"
 
 # Update or set the LEVEL_ZERO_SDK_V<major_version>_PATH environment variable
 $majorVersion = $sdkVersion.Split('.')[0]
-$envVarName = "LEVEL_ZERO_SDK_V${majorVersion}_PATH"
+$envVarName = "LEVEL_ZERO_V${majorVersion}_SDK_PATH"
 $currentEnvPath = [System.Environment]::GetEnvironmentVariable($envVarName, [System.EnvironmentVariableTarget]::Machine)
 if ($null -eq $currentEnvPath -or $currentEnvPath -notcontains $sdkPath) {
     [System.Environment]::SetEnvironmentVariable($envVarName, $sdkPath, [System.EnvironmentVariableTarget]::Machine)
