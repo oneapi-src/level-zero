6772a6f65adea0d03fe30784b45e6247ed626b04
Neil R. Spruit <neil.r.spruit@intel.com>
2025-03-06 13:47:40 -0800

    Remove the base stype validator such that only specialized functions have the check (#291)
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- source/layers/validation/checkers/parameter_validation/param_validation.h
+++ source/layers/validation/checkers/parameter_validation/param_validation.h
@@ -30,18 +30,6 @@ namespace validation_layer
 
     template <typename T>
     ze_result_t ParameterValidation::validateExtensions(T descriptor){
-        const ze_base_properties_t *base = reinterpret_cast<const ze_base_properties_t*> (descriptor);
-        while(base){
-            if((base->stype > ZE_STRUCTURE_TYPE_EXTERNAL_MEMORY_EXPORT_WIN32 && 
-               base->stype < ZE_STRUCTURE_TYPE_DEVICE_RAYTRACING_EXT_PROPERTIES) || 
-               (base->stype > ZE_STRUCTURE_TYPE_IMAGE_VIEW_PLANAR_EXT_DESC && 
-               base->stype < ZE_STRUCTURE_TYPE_RELAXED_ALLOCATION_LIMITS_EXP_DESC) ||
-               (base->stype > ZE_STRUCTURE_TYPE_MEMORY_SUB_ALLOCATIONS_EXP_PROPERTIES &&
-                base->stype < ZE_STRUCTURE_TYPE_MUTABLE_COMMAND_LIST_EXP_PROPERTIES) ||
-               base->stype > ZE_STRUCTURE_TYPE_INIT_DRIVER_TYPE_DESC)
-                return ZE_RESULT_ERROR_INVALID_ARGUMENT;
-            base = reinterpret_cast<const ze_base_properties_t*>(base->pNext);
-        }
         return ZE_RESULT_SUCCESS;
     }
 
