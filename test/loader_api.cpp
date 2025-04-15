f5816a4f52db2cbce155e68002fbb72458e40fea
Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>
2025-04-15 12:43:47 +0200

    test: ensure no stdout output when calling zeInitDrivers
    Signed-off-by: Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>

--- test/loader_api.cpp
+++ test/loader_api.cpp
@@ -12,9 +12,20 @@
 #include "ze_api.h"
 #include "zes_api.h"
 
+#include <fstream>
+
 #if defined(_WIN32)
+    #include <io.h>
+    #include <cstdio>
+    #include <fcntl.h>
     #define putenv_safe _putenv
 #else
+    #include <cstdlib>
+    #include <sys/types.h>
+    #include <unistd.h>
+    #define _dup dup
+    #define _dup2 dup2
+    #define _close close
     #define putenv_safe putenv
 #endif
 
@@ -363,6 +374,70 @@ TEST(
 
   EXPECT_EQ(ZE_RESULT_SUCCESS, zeInit(0));
   EXPECT_FALSE(zelCheckIsLoaderInTearDown());
+}
+
+class CaptureOutput {
+private:
+    int original_fd;
+    int fd;
+    int stream;
+    char filename[50] = "/tmp/capture_output_XXXXXX";
+
+public:
+    enum { Stdout = 1, Stderr = 2 };
+
+    CaptureOutput(int stream_) : stream(stream_) {
+        original_fd = _dup(stream); 
+#if defined(__linux__)
+        fd = mkstemp(filename);
+#elif defined(_WIN32)
+        tmpnam_s(filename, sizeof(filename));
+        _sopen_s(&fd, filename, _O_CREAT | _O_RDWR, _SH_DENYNO, _S_IREAD | _S_IWRITE);
+#endif
+        fflush(nullptr);
+        _dup2(fd, stream);
+        _close(fd);
+    }
+
+    ~CaptureOutput() {
+        if (original_fd != -1) {
+            fflush(nullptr);
+            _dup2(original_fd, stream);
+            _close(original_fd);
+            original_fd = -1;
+        }
+        if (remove(filename) != 0) {
+            std::cerr << "Deleting file " << filename << " failed.";
+        }
+    }
+
+    std::string GetOutput() {
+        if (original_fd != -1) {
+            fflush(nullptr);
+            _dup2(original_fd, stream);
+            _close(original_fd);
+            original_fd = -1;
+        }
+        std::ifstream stream(filename);
+        std::string output = std::string((std::istreambuf_iterator<char>(stream)),
+            std::istreambuf_iterator<char>());
+        return output;
+    }
+};
+
+TEST(
+    LoaderInitDrivers,
+    GivenZeInitDriverWhenCalledThenNoOutputIsPrintedToStdout) {
+    uint32_t pInitDriversCount = 0;
+    ze_init_driver_type_desc_t desc = { ZE_STRUCTURE_TYPE_INIT_DRIVER_TYPE_DESC };
+    desc.flags = UINT32_MAX;
+    desc.pNext = nullptr;
+
+    CaptureOutput capture(CaptureOutput::Stdout);
+    EXPECT_EQ(ZE_RESULT_SUCCESS, zeInitDrivers(&pInitDriversCount, nullptr, &desc));
+    
+    std::string output = capture.GetOutput();
+    EXPECT_TRUE(output.empty());
 }
 
 } // namespace
