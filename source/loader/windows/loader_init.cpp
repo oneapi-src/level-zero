248d6dfba6677c2e27969646bac196658679e079
Neil R. Spruit <neil.r.spruit@intel.com>
2025-11-04 07:49:01 -0800

    Add documentation on dllmain restrictions (#389)
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

--- source/loader/windows/loader_init.cpp
+++ source/loader/windows/loader_init.cpp
@@ -11,6 +11,12 @@
 namespace loader
 {
 #ifdef L0_STATIC_LOADER_BUILD
+    /*
+    * DllMain is called by the OS when the DLL is loaded or unloaded.
+    * When modifying the code here, be aware of the restrictions on what can be done
+    * inside DllMain. See https://learn.microsoft.com/en-us/windows/win32/dlls/dynamic-link-library-best-practices
+    * for more information.
+    */
     extern "C" BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
         if (fdwReason == DLL_PROCESS_DETACH) {
             delete context;
