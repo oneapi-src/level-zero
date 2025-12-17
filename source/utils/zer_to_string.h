7427bc8a2f66eec09f7dd6e4ee8d9a8fde9a96e8
Neil R. Spruit <neil.r.spruit@intel.com>
2025-12-17 08:03:50 -0800

    Update the tracing of APIs in the validation layer with params
    Signed-off-by: Neil R. Spruit <neil.r.spruit@intel.com>

---
+++ source/utils/zer_to_string.h
@@ -0,0 +1,29 @@
+/*
+ * ***THIS FILE IS GENERATED. ***
+ * See to_string.h.mako for modifications
+ *
+ * Copyright (C) 2025 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * @file zer_to_string.h
+ * 
+ * to_string functions for Level Zero types
+ */
+
+#ifndef _ZER_TO_STRING_H
+#define _ZER_TO_STRING_H
+
+#include "ze_api.h"
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+// Include ze_to_string.h for common definitions
+#include "ze_to_string.h"
+
+namespace loader {
+// Struct to_string functions
+} // namespace loader
+
+#endif // _ZER_TO_STRING_H
